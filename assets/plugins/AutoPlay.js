cf370038d3dbdf511851fb518acbb6aa2b4cd322
ingrid <msagrerom@inee.edu.mx>
2019-09-18 17:35:50 -0500

    hasta getters y setters

--- assets/plugins/AutoPlay.js
+++ assets/plugins/AutoPlay.js
@@ -1,8 +1,9 @@
 function AutoPlay() {}
+  AutoPlay.prototype.run = function (player) {
+    if(!player.muted) {
+      player.muted = true;
+    }
+    player.play();
+  };
 
-AutoPlay.prototype.run = function (player) {
-  player.mute();
-  player.togglePlay();
-};
-
-export default AutoPlay;
+  export default AutoPlay;
